e92d483b6bb606a565ee02904077749936d81a79
hanseh <hans.torres@98labs.com>
2014-05-10 20:26:51 +0800

    heartbeat wp initial commit

---
+++ HeartBeat/HeartBeat/Info.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace HeartBeat
+{
+    public partial class info : PhoneApplicationPage
+    {
+        public info()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationBarPulseIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        private void ApplicationBarInfoIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/Info.xaml", UriKind.Relative));
+        }
+
+        private void ApplicationBarSettingIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/Settings.xaml", UriKind.Relative));
+        } 
+    }
+}
---
+++ HeartBeat/HeartBeat/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace HeartBeat
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            button1.Visibility = Visibility.Collapsed;
+            button2.Visibility = Visibility.Visible;
+            beat.Begin();
+        }
+
+        private void button1_Click_2(object sender, RoutedEventArgs e)
+        {
+            button2.Visibility = Visibility.Collapsed;
+            button1.Visibility = Visibility.Visible;
+            beat.Stop();
+        }
+
+        private void ApplicationBarPulseIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        private void ApplicationBarInfoIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/Info.xaml", UriKind.Relative));
+        }
+
+        private void ApplicationBarSettingIconButton_Click(object sender, EventArgs e)
+        {
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/Settings.xaml", UriKind.Relative));
+        } 
+    }
+}
